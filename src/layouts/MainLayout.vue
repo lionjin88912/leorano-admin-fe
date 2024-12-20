0def032588468e03c960dfce02138440a2b1614c
sunkuo <sunkuo@liontravel.com>
2024-12-20 15:58:35 +0800

    [moidfy]修正頁面標題

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,7 @@
     <q-header elevated class="bg-white text-grey-8 q-py-xs" height-hint="50">
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
-        <q-toolbar-title> Le Orano Console </q-toolbar-title>
+        <q-toolbar-title> Le Oràno Console </q-toolbar-title>
       </q-toolbar>
     </q-header>
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered :width="200">
